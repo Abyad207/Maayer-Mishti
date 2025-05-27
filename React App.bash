5d5fa822e39ef7a12537908a522e3435aafd9341
Abyad207 <abyadkhan207@gmail.com>
2025-05-27 00:49:05 -0400

    Update and rename React App to React App.bash

---
+++ React App.bash
@@ -0,0 +1,15 @@
+npx create-react-app maayer-mishti
+cd maayer-mishti
+
+npm install react-icons
+
+npm start
+
+npm install gh-pages --save-dev
+
+git add .
+git commit -m "Configure deployment to GitHub Pages"
+git push origin main
+
+npm run deploy
+
